0e15f0d4547271900bff47d73f2e6f7588614c48
Craftman <craftsman@dhatvibs.com>
2026-02-18 10:06:28 +0530

    testing 1

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        sucess : true,
+        message : "testing"
+    })
+})
+
+app.listen(4545 ,()=>{
+    console.log('server start....')
+})
